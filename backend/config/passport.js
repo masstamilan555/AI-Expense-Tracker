1b4337c7890c3fb3fb36fb483e95fffe3947a866
Tamilarasu V <165547564+masstamilan555@users.noreply.github.com>
2025-05-20 19:26:30 +0530

    Update passport

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -10,7 +10,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:4000/api/auth/google/callback" 
+            callbackURL: "https://expense-tracker-api-rmjc.onrender.com/auth/google/callback" 
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
